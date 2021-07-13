885e9cfb2fe6981cedc68a1c8efc22f88a44a152
Cyanox <alexis.moyse@gmail.com>
2021-07-13 17:20:31 +0200

    Ajout de la liste des produits

--- php/products.php
+++ php/products.php
@@ -79,7 +79,7 @@
 
 <div class="container">
     <div class="row">
-        <main class="col-md-12 ms-sm-auto col-lg-6 px-md-4">
+        <main class="col-md-12 ms-sm-auto col-lg-6">
             <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
                 <h1 class="h2">Produits</h1>
             </div>
