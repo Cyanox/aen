0ee973dcd0f13aaa7ea3e3b5820be1e63c8ada06
Cyanox <alexis.moyse@gmail.com>
2021-07-13 11:51:14 +0200

    deploiement test

--- index.php
+++ index.php
@@ -31,7 +31,7 @@
 <body>
 
 <header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
-    <img class="navbar-brand col-md-3 col-lg-2 me-0 px-3" src="/assets/image/Logo_LEP_NOBG.png" alt="" width="288" height="228">
+    <img class="navbar-brand col-md-3 col-lg-2 me-0 px-3" src="/assets/image/Logo_LEP_NOBG.png" alt="" width="72" height="57">
     <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
