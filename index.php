df6741f0e5190a0ce075357abda13d4b2ff9c058
Cyanox <alexis.moyse@gmail.com>
2021-07-13 16:10:18 +0200

    deploiement test

--- index.php
+++ index.php
@@ -80,7 +80,7 @@
 <div class="container-fluid">
     <div class="row">
         <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-            <div class="d-flex">
+            <div class="d-flex justify-content-between flex-wrap align-items-center pt-3 pb-2 mb-3 border-bottom">
                 <h1 class="h2">Accueil</h1>
                 <div class="btn-toolbar mb-2 mb-md-0">
                     <div class="btn-group me-2">
