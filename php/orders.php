72f154569d12fca9c358a59f330a9502bd42f902
Cyanox <alexis.moyse@gmail.com>
2021-07-13 16:29:03 +0200

    deploiement test

--- php/orders.php
+++ php/orders.php
@@ -81,7 +81,7 @@
     <div class="row">
         <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
             <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-                <h1 class="h2">Accueil</h1>
+                <h1 class="h2">Commandes</h1>
                 <div class="btn-toolbar mb-2 mb-md-0">
                     <div class="btn-group me-2">
                         <button type="button" class="btn btn-sm btn-outline-success"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-plus-circle-dotted" viewBox="0 0 16 16">
