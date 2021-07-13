18e4791a78cf967b3d3ffbf6f8db9d1a0a962a0d
Cyanox <alexis.moyse@gmail.com>
2021-07-13 15:53:50 +0200

    deploiement test

--- index.php
+++ index.php
@@ -33,8 +33,8 @@
     <div class="container">
         <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
             <a href="/" class="d-flex align-items-center my-2 my-lg-0 me-lg-auto text-white text-decoration-none">
-                <img src="/assets/image/Logo_LEP_NOBGv2.png" alt="" width="72" height="57">
-                <span class="ms-4">L'Étanchéité Parisienne</span>
+                <img src="/assets/image/Logo_LEP_NOBGv2.png" alt="" class="pe-4" width="72" height="57">
+                <span class="fs-5">L'Étanchéité Parisienne</span>
             </a>
 
             <ul class="nav col-12 col-lg-auto my-2 justify-content-center my-md-0 text-small">
