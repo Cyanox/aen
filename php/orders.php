6ca0024a89831d4f2e9647a7f8b14a3fb3910301
Cyanox <alexis.moyse@gmail.com>
2021-08-18 14:17:46 +0200

    first push

--- php/orders.php
+++ php/orders.php
@@ -107,7 +107,7 @@ require_once __DIR__ . '/../api/dao/lep.php'; ?>
                     <th scope="col">Emetteur</th>
                     <th scope="col">Récépteur</th>
                     <th scope="col">Date</th>
-                    <th scope="col"><a href="productadd.php" type="button"
+                    <th scope="col"><a href="orderadd.php" type="button"
                                        class="btn btn-sm align-items-center btn-outline-success">
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor"
                                  class="bi bi-plus-circle-dotted" viewBox="0 0 16 16">
@@ -130,7 +130,7 @@ require_once __DIR__ . '/../api/dao/lep.php'; ?>
                                     <td>' . $recepter . '</td>
                                     <td>' . $date . '</td>
                                     <td><div class="btn-group" role="group" aria-label="Basic mixed styles example">
-                                            <a href="ordersee.php?ref=' . $id . '" class="btn btn-sm btn-outline-info"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-eye" viewBox="0 0 16 16">
+                                            <a href="orderview.php?ref=' . $id . '" class="btn btn-sm btn-outline-info"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-eye" viewBox="0 0 16 16">
   <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z"/>
   <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z"/>
 </svg></a>
